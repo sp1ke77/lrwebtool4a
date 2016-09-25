c68001a3438279931612cf2bc78bbfb34054950b
Isaque <sp1ke77@gmail.com>
2016-09-26 00:34:16 +0100

    137 - sindi

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', ['ngSanitize','720kb.socialshare'])
+angular.module('starter.controllers', ['ngSanitize','720kb.socialshare','ionic'])
 
 .controller('AppCtrl', function($scope, $rootScope, $state) {
 
@@ -147,7 +147,7 @@ angular.module('starter.controllers', ['ngSanitize','720kb.socialshare'])
 
 })
 
-.controller('SindiCtrl', function($rootScope, $scope, $filter) {
+.controller('SindiCtrl', function($rootScope, $scope, $filter,$ionicPopup, $timeout) {
 	
 	var deviceInformation = ionic.Platform.device();
 
@@ -226,6 +226,75 @@ angular.module('starter.controllers', ['ngSanitize','720kb.socialshare'])
 	
 	if (isIOS) { $scope.htmlsms6 	= 'sms:&body=http://lrwebtool.com/'+$rootScope.slug6+'/?id='+$rootScope.username+' '+$rootScope.titulo6;} 
 	if (isAndroid) { $scope.htmlsms6 	= 'sms:?body=http://lrwebtool.com/'+$rootScope.slug6+'/?id='+$rootScope.username+' '+$rootScope.titulo6;}
+    
+    
+    
+    // Triggered on a button click, or some other target
+$scope.showPopup = function() {
+  $scope.data = {};
+
+  // An elaborate, custom popup
+  var myPopup = $ionicPopup.show({
+    template: '<a class="crunchify-link crunchify-facebook" href="https://www.facebook.com/sharer/sharer.php?u=http://lrwebtool.com/'+$rootScope.slug6+'/?id='+$rootScope.username+'" >Facebook</a>',
+    title: 'Enter Wi-Fi Password',
+    subTitle: 'Please use normal things',
+    scope: $scope,
+    buttons: [
+      { text: 'Cancel' },
+      {
+        text: '<b>Save</b>',
+        type: 'button-positive',
+        onTap: function(e) {
+          if (!$scope.data.wifi) {
+            //don't allow the user to close unless he enters wifi password
+            e.preventDefault();
+          } else {
+            return $scope.data.wifi;
+          }
+        }
+      }
+    ]
+  });
+
+  myPopup.then(function(res) {
+    console.log('Tapped!', res);
+  });
+
+  $timeout(function() {
+     //myPopup.close(); //close the popup after 3 seconds for some reason
+  }, 3000);
+ };
+
+ // A confirm dialog
+ $scope.showConfirm = function() {
+   var confirmPopup = $ionicPopup.confirm({
+     title: 'Consume Ice Cream',
+     template: 'Are you sure you want to eat this ice cream?'
+   });
+
+   confirmPopup.then(function(res) {
+     if(res) {
+       console.log('You are sure');
+     } else {
+       console.log('You are not sure');
+     }
+   });
+ };
+
+ // An alert dialog
+ $scope.showAlert = function() {
+   var alertPopup = $ionicPopup.alert({
+     title: 'Don\'t eat that!',
+     template: 'It might taste good'
+   });
+
+   alertPopup.then(function(res) {
+     console.log('Thank you for not eating my delicious ice cream cone');
+   });
+ };
+    
+    
+    
       	
 })
 	
