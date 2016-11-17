0403f969b8b4ab0cd65232c63d7d108cc0dabc2c
Isaque Fernandes <sp1ke77@gmail.com>
2016-11-17 22:35:08 +0000

    159.61

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,23 @@ angular.module('starter', ['ionic','starter.controllers','ngCordova'])
 .run(function($ionicPlatform, $rootScope, $cordovaGlobalization) {
 
 $ionicPlatform.ready(function() {
+
+
+	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+      cordova.plugins.Keyboard.disableScroll(false);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+
+
+
+
 			if (window.cordova) {
 					navigator.globalization.getPreferredLanguage(
 					  function (language) {
