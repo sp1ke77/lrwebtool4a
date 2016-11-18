b56bbd0996b7f8f23c80e08165d86f6aa9cdf7f5
Isaque Fernandes <sp1ke77@gmail.com>
2016-11-18 02:31:13 +0000

    159.77

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -529,7 +529,7 @@ $ionicPlatform.ready(function() {
 						console.log('REGISTERCTRL 6: ' + $rootScope.profilename);
 
 							if(alert("DISPOSITIVO ATIVADO\nUtilizador: " + localStorage.getItem("username") + "\nChave/Clave: " + localStorage.getItem("token"))){ }
-							else window.location.reload(); $rootScope.language = localStorage.getItem('language'); console.log('REGISTERCTRL exit language: ' + $rootScope.language); $state.go('app.intro');
+							else $state.go('app.intro');
 						}
 
 					});
